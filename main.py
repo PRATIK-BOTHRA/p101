3946c7c708bb22ae46324c10b0c50636495193e2
PRATIK-BOTHRA <74529643+PRATIK-BOTHRA@users.noreply.github.com>
2022-03-02 16:54:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
-import random
+import random 
+  
 
-#range of the values of a dice
-min_val = 1
-max_val = 6
+response = "y"
+   
+while response == "y": 
+       
+    no = random.randint(1,6) 
 
-#to loop the rolling through user input
-roll_again = "yes"
-
-#loop
-while roll_again == "yes" or roll_again == "y":
-    print("Rolling The Dices...")
-    print("The Values are :")
-    
-    #generating and printing 1st random integer from 1 to 6
-    print(random.randint(min_val, max_val))
-    
-    #generating and printing 2nd random integer from 1 to 6
-    print(random.randint(min_val, max_val))
-    
-    #asking user to roll the dice again. Any input other than yes or y will terminate the loop
-    roll_again = input("Roll the Dices Again?") 
+    if no == 1: 
+        print("[-----]") 
+        print("[     ]") 
+        print("[  0  ]") 
+        print("[     ]") 
+        print("[-----]") 
+    if no == 2: 
+        print("[-----]") 
+        print("[ 0   ]") 
+        print("[     ]") 
+        print("[   0 ]") 
+        print("[-----]") 
+    if no == 3: 
+        print("[-----]") 
+        print("[     ]") 
+        print("[0 0 0]") 
+        print("[     ]") 
+        print("[-----]") 
+    if no == 4: 
+        print("[-----]") 
+        print("[0   0]") 
+        print("[     ]") 
+        print("[0   0]") 
+        print("[-----]") 
+    if no == 5: 
+        print("[-----]") 
+        print("[0   0]") 
+        print("[  0  ]") 
+        print("[0   0]") 
+        print("[-----]") 
+    if no == 6: 
+        print("[-----]") 
+        print("[0 0 0]") 
+        print("[     ]") 
+        print("[0 0 0]") 
+        print("[-----]") 
+          
+       
+    response=input("press y to roll again and n to exit:") 
+    print("\n") 
